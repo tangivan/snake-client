8d37cab3bf870acf8df37d78266f5b228df3f7d3
Ivan <ivanxintang@gmail.com>
2021-11-03 12:34:53 -0700

    Added my initials to the game when I connect

--- client.js
+++ client.js
@@ -13,6 +13,11 @@ const connect = function() {
     console.log('Server says: ', data);
   });
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server.');
+    conn.write("Name: IT");
+  })
+
   return conn;
 };
 
