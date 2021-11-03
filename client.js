8ef78665990d42c27bce90d8d2694b5e2b1fdda2
Ivan <ivanxintang@gmail.com>
2021-11-03 12:59:58 -0700

    added stdin inputs and handling

--- client.js
+++ client.js
@@ -16,22 +16,14 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server.');
     conn.write("Name: IT");
-    let delay = 0;
-    let moves = ["Move: up", "Move: up", "Move: right", "Move: up", "Move: down"];
-    // for (let i = 0; i < 5; i++) {
-    //   setTimeout(() => {
-    //     conn.write(moves[i]);
-    //   }, delay * 1000)
-    //   delay += 100;
-    // }
-    let counter = 0;
-    let id = setInterval(() => {
-      conn.write("Move: up")
-      counter++;
-      if (counter === 5) {
-        clearInterval(id);
-      }
-    }, 500);
+    // let counter = 0;
+    // let id = setInterval(() => {
+    //   conn.write("Move: up")
+    //   counter++;
+    //   if (counter === 5) {
+    //     clearInterval(id);
+    //   }
+    // }, 500);
   })
 
   return conn;
--- play.js
+++ play.js
@@ -2,3 +2,22 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // your code here
+  console.log(key);
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin = setupInput();
+stdin.on("data", handleUserInput);
