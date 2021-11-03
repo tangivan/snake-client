4403bd1eae0cc5d14d994c3070699b2263f8dede
Ivan <ivanxintang@gmail.com>
2021-11-03 13:08:11 -0700

    modularized input logic into input.js and imported it into play.js

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  // your code here
-  console.log(key);
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-stdin = setupInput();
-stdin.on("data", handleUserInput);
+setupInput();
